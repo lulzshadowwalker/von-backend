88223eaafbd9fbe2abf3102ad24d2c9999fba487
lulzshadowwalker <lulzshadowwalker@gmail.com>
2024-11-22 16:59:34 +0300

    Updated /auth/forgot-password payload's to match the other apis

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -18,14 +18,14 @@ class PasswordResetLinkController extends ApiController
     public function store(Request $request)
     {
         $request->validate([
-            'email' => ['required', 'email'],
+            'data.attributes.email' => ['required', 'email'],
         ]);
 
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
         $status = Password::sendResetLink(
-            $request->only('email')
+            ['email' => $request->input('data.attributes.email')]
         );
 
         if ($status != Password::RESET_LINK_SENT) {
